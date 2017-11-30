a94a8e7dfaae00d418bcf7b0e3615b39a0b29840
popuplabus <34142817+popuplabus@users.noreply.github.com>
2017-11-30 16:49:08 -0500

    delete hello belfast

--- src/HelloBelfast.java
+++
@@ -1,6 +0,0 @@
-public class HelloPortsmouth {
-   public static void main(String[] args) {
-      // Prints "Hello, Portsmouth!" in the terminal window.
-      System.out.println("Hello, Portsmouth!");
-   }
-}
